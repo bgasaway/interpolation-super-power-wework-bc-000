385a795089defdc1f70735c02a223261ae888fbc
Brooke Gasaway <brooke.gasaway@wework.com>
2018-02-13 17:57:19 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,5 @@
+def display_rainbow(color)
+ puts"R: #{color[0]}, O:#{color[1]}, Y:#{color[2]}, G:#{color[3]}, B:#{color[4]}, I: #{color[5]"
+color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+end
+display_rainbow(color)
